679f59e9e87cf9f76b0ecd46a75231ef7e3cb9b3
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2023-05-19 22:22:44 -0400

    rest

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_030541) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_015246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "username"
-    t.string "password_digest"
-    t.string "session_token"
+    t.string "email", null: false
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
