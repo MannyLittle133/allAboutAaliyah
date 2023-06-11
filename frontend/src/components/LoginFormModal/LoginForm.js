92ce3035f36a25b230eda5c9dbf4569f11222e93
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2023-06-10 21:42:33 -0400

    night

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,18 +1,14 @@
 
-import React, { useState } from 'react';
-import * as sessionActions from '../../store/session';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import './LoginForm.css';
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch } from "react-redux";
+import "./LoginForm.css";
 
-function LoginFormPage() {
+function LoginForm() {
   const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
-  const [credential, setCredential] = useState('');
-  const [password, setPassword] = useState('');
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
-
-  if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,7 +26,7 @@ function LoginFormPage() {
         else if (data) setErrors([data]);
         else setErrors([res.statusText]);
       });
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -60,4 +56,4 @@ function LoginFormPage() {
   );
 }
 
-export default LoginFormPage;
+export default LoginForm;
